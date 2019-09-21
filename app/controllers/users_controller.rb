1ecac3ed09c6fc2fec7ecdf2ae6c38ed15230c26
akiyoshi_okumura <akiyoshia.m.1231@gmail.com>
2019-09-21 16:06:21 +0900

    finish sample_app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,17 @@ class UsersController < ApplicationController
     redirect_to action: :index
   end
 
-  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to action: :index
+  end
+
   
     private 
 
